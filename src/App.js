831b5e73188aef85eae7181c937c1515713437c9
Kūhaku <alessandro.benvenuti09@gmail.com>
2022-11-11 04:15:03 -0500

    Added bar for divide software works from games

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import React,{ useState } from 'react';
 import './App.css';
 import { Project } from './components/Projects';
 
-function App() {
 
+function App() {
+  const [softShow, setSoftShow] = useState(true);
+  const [gameShow, setGameShow] = useState(true);
+  function softClick() {
+    setSoftShow(true);
+    setGameShow(false);
+    document.getElementsByClassName("SoftwareBtn").item(0).style.backgroundColor= "#2b2b46";
+    document.getElementsByClassName("GameBtn").item(0).style.backgroundColor= "#09090e";
+  }
+  function gameClick() {
+    document.getElementsByClassName("GameBtn").item(0).style.backgroundColor= "#2b2b46";
+    document.getElementsByClassName("SoftwareBtn").item(0).style.backgroundColor= "#09090e";
+    setSoftShow(false);
+    setGameShow(true);
+  }
   return (
     <div className="App">
       <div className="box">
@@ -25,26 +40,46 @@ function App() {
           </div>   
         </div>
         <hr></hr>
+        <div className="bar">
+          <ul className="button-list">
+            <button type="button" className="SoftwareBtn" onClick={softClick}>Softwares</button>
+            <button type="button" className="GameBtn"onClick={gameClick}>Games</button>
+          </ul>
+        </div>
         <ul>
           <p className="work-text">Works:</p>
-          <Project 
-            projectName = "R6 Tracker"
-            imageUrl = "R6Tracker"
-            link = "https://github.com/CardinalSys/R6TrackerV2"
-            projectDesc = "R6 Tracker, designed for TinoRed (Cuba intranet) is a program for check your statistics in Rainbow Six Siege LAN mode by reading the game's RAM in real time."
-          />
-            <Project 
-            projectName = "Nauta Manager"
-            imageUrl = "NautaManager"
-            link = "https://github.com/CardinalSys/NautaManager"
-            projectDesc = "Nauta Manager is a program to connect to Nauta Hogar without using the web, with a tool to schedule the disconnection, a connection quality checker and an option to disable the automatic disconnection every 12 hours."
-          />
+          {
+            softShow && <div className="SoftwareWorks">
+              <Project 
+                projectName = "R6 Tracker"
+                imageUrl = "R6Tracker"
+                link = "https://github.com/CardinalSys/R6TrackerV2"
+                projectDesc = "R6 Tracker, designed for TinoRed (Cuba intranet) is a program for check your statistics in Rainbow Six Siege LAN mode by reading the game's RAM in real time."
+              />
+                <Project 
+                projectName = "Nauta Manager"
+                imageUrl = "NautaManager"
+                link = "https://github.com/CardinalSys/NautaManager"
+                projectDesc = "Nauta Manager is a program to connect to Nauta Hogar without using the web, with a tool to schedule the disconnection, a connection quality checker and an option to disable the automatic disconnection every 12 hours."
+              />
+            </div>
+          }
+          {
+            gameShow && <div className="GameWorks">
+              <Project 
+                  projectName = "Space War"
+                  imageUrl = "SpaceWar"
+                  link = "https://cardinalsys.itch.io/spacewar"
+                  projectDesc = "SpaceWar is a simple 2D Space Shooter Game made with GitHubCopilot in Unity."
+                />
+            </div>
+          }
         </ul>
         <footer className="footer">
         <hr></hr>
           <div class="logo-container">
             <a href ="http://github.com/CardinalSys" target="_blank" rel="noopener noreferrer"> <img src={require("./img/GitHub.png")} class="logo" alt="GitHub logo"></img></a>
-            <a href="https://mail.google.com/mail/u/0/?view=cm&fs=1&to=alessandro.benvenuti09@gmail.com&tf=1" target="_blank" rel="noopener noreferrer"> <img src={require("./img/gmail.png")} class="logo" alt="Gmail logo"></img></a>
+            <a href="https://mail.google.com/mail/u/0/?view=cm&fs=1&to=cardinalsys00@gmail.com&tf=1" target="_blank" rel="noopener noreferrer"> <img src={require("./img/gmail.png")} class="logo" alt="Gmail logo"></img></a>
           </div>
         </footer>
       </div>
